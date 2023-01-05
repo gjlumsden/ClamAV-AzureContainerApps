0372c1b2218d3b2e0334ffeb85c43b25c01290e7
Gary Lumsden <galumsde@microsoft.com>
2023-01-05 15:36:13 +0000

    Fix loading of user secrets.

--- PoC.VirusScan.Function/Program.cs
+++ PoC.VirusScan.Function/Program.cs
@@ -1,19 +1,32 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using nClam;
 
-//Add to local.settings.json or user secrets.
-
-string serverName = Environment.GetEnvironmentVariable("AvScanEndpointUrl");
-
-//Update if a different exposed port is used.
-int serverPort = 3310;
+IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Environment.CurrentDirectory)
+               .AddEnvironmentVariables()
+               .AddUserSecrets<Program>()
+               .Build();
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
-        services.AddSingleton<IClamClient, ClamClient>(factory => new ClamClient(serverName, serverPort));
+        services.AddSingleton<IClamClient, ClamClient>(factory =>
+        {
+            //Add to local.settings.json or user secrets.
+            string? serverName = configuration["AvScanEndpointUrl"];
+            
+            if(serverName == null)
+            {
+                throw new Exception("AvScanEndpointUrl setting is not set.");
+            }
+            //Update if a different exposed port is used.
+            int serverPort = 3310;
+            
+            return new ClamClient(serverName, serverPort);
+        });
     })
     .Build();
 
